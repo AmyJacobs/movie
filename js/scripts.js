0e090dd8e1aaec40bb229e7ff454302e68450c27
Amelia Jacobs and andrea wilkowski <amelia@>
2017-11-27 20:09:24 -0800

    Add prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,57 @@
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var title = $("#title").val();
-    var time = $("#time").val();
-    var age = parseInt($("input#age").val());
+function Movie(title, time, age, price) {
+  this.title = title;
+  this.time = time;
+  this.age = age;
+  this.price = price;
+}
 
-    console.log(title + ", " + time + ", " + age);
+// Movie.prototype.type = function() {
+//   if (age < 18) {
+//     return "Child";
+//   } else if (age > 65) {
+//     return "Senior";
+//   } else {
+//     return "Adult";
+//   }
+// }
 
-
-   if (age < 18) {
-    var tickType = "Child";
-    var price = 15;
-  } else if (age > 65) {
-    var tickType = "Senior";
-    var price = 10;
+Movie.prototype.priceCalc = function() {
+  if (Movie.age < 18) {
+    price = 15;
+  } else if (Movie.age > 65) {
+    price = 10;
   } else {
-    var tickType = "Adult";
-    var price = 20;
+    price = 20;
   }
 
   if (time === "8AM-2PM") {
     price -= 5;
   }
+  return price;
+}
 
 
-    $(".title-output").text(title);
-    $(".age-output").text(tickType);
-    $(".time-output").text(time);
-    $(".price-output").text(price);
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+    var title = $("#title").val();
+    var time = $("#time").val();
+    var age = parseInt($("input#age").val());
+    var price = 0;
+
+    var myMovie = new Movie(title,time,age,price);
+
+    console.log(myMovie);
+
+
+    $(".title-output").text(myMovie.title);
+    $(".age-output").text(myMovie.age);
+    $(".time-output").text(myMovie.time);
+    $(".price-output").text(myMovie.priceCalc(price));
 
     $("#output").show();
 
 
-    event.preventDefault();
+
   });
 });
